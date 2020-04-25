8751ea91fdf0d2f66124521b79c70211b11d28b3
Moustafa Kandeel <moustafa.kandeel@signavio.com>
2020-04-25 10:45:08 +0200

    add argocd clusterrolebinding and argo cd app of apps

--- cluster/cluster_config/cluster_config.tf
+++ cluster/cluster_config/cluster_config.tf
@@ -1,6 +1,7 @@
 resource "helm_release" "cluster_config" {
-  name  = "k8s-config" # make sure name follows helm chart name rules
-  chart = "./${path.module}/k8s_config"
+  name    = "k8s-config" # make sure name follows helm chart name rules
+  chart   = "./${path.module}/k8s_config"
+  timeout = 600 # workaround
 }
 
 output "cluster_config_staus" {
